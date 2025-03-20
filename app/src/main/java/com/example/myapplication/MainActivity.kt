58c1a3b19bad8a331aae1bee0a0ca4d613d83dda
Jialin <jiajy027@gmail.com>
2025-03-20 22:04:24 +1100

    added splash screen, in progress improve the dynamic year blocks displaying, done

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,34 +4,59 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.navigation.NavHostController
 import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.material.MaterialTheme.colors
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.rounded.AccountCircle
 import androidx.compose.material.icons.rounded.Menu
 import androidx.compose.material.icons.rounded.Notifications
+import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.delay
+import java.util.Calendar
 
 
 class MainActivity : ComponentActivity() {
@@ -44,74 +69,14 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CalendarApp() {
-
-//    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(
-//        state = rememberTopAppBarState()
-//    )
-
-    Scaffold /*(
-//        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-//
-//        topBar = {
-//            TopBar(scrollBehavior = scrollBehavior)
-//
-////            CenterAlignedTopAppBar(
-////                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-////                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-////                    titleContentColor = MaterialTheme.colorScheme.primary,
-////                ),
-////                title = {
-////                    Text(
-////                        "Centered Top App Bar",
-////                        maxLines = 1,
-////                        overflow = TextOverflow.Ellipsis
-////                    )
-////                },
-////                navigationIcon = {
-////                    IconButton(onClick = { /* do something */ }) {
-////                        Icon(
-////                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-////                            contentDescription = "Localized description"
-////                        )
-////                    }
-////                },
-////                actions = {
-////                    IconButton(onClick = { /* do something */ }) {
-////                        Icon(
-////                            imageVector = Icons.Filled.Menu,
-////                            contentDescription = "Localized description"
-////                        )
-////                    }
-////                },
-////                scrollBehavior = scrollBehavior,
-////            )
-//        },
-        bottomBar = {
-
-        },
-        floatingActionButton = {
-
-        }
-        ) */ {
+    Scaffold{
         paddingValues ->
         Screen(
             modifier = Modifier.padding(paddingValues)
         )
     }
-
-//    val navController = rememberNavController()
-//
-////     code to show year grid and month view
-//    NavHost(navController, startDestination = "year_grid") {
-//        composable("year_grid") { YearGridScreen(navController) }
-//        composable("month_view/{year}") { backStackEntry ->
-//            val year = backStackEntry.arguments?.getString("year")
-//            MonthViewScreen(year)
-//        }
-//    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -126,34 +91,59 @@ fun TopBar(
             .clip(RoundedCornerShape(100.dp)),
         scrollBehavior = scrollBehavior,
         colors = TopAppBarDefaults.topAppBarColors(
-//            containerColor = MaterialTheme.colorScheme.surfaceVariant.copy(0.6f)
             containerColor = MaterialTheme.colorScheme.primaryContainer,
-//            titleContentColor = MaterialTheme.colorScheme.surfaceVariant.copy(0.6f),
         ),
         windowInsets = WindowInsets(top = 0.dp),
         title = {
-            Text(
-                text = "Sreach your notes",
-                color = MaterialTheme.colorScheme.onBackground.copy(0.7f),
-                fontSize = 17.sp
+            TextField(
+                value = "",
+                onValueChange = { /* update search query */},
+                placeholder = @androidx.compose.runtime.Composable {
+                    Text(
+                        text = "Search your notes",
+                        color = MaterialTheme.colorScheme.onBackground.copy(0.7f),
+                        fontSize = 17.sp
+                    )
+                },
+                modifier = Modifier.fillMaxWidth(),
+                colors = TextFieldDefaults.colors(
+                    focusedContainerColor = Color.Transparent,
+                    unfocusedContainerColor = Color.Transparent,
+                    disabledContainerColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    disabledIndicatorColor = Color.Transparent
+                )
             )
         },
         navigationIcon = {
             Icon(
                 imageVector = Icons.Rounded.Menu,
-                contentDescription = null,
+                contentDescription = "Open navigation menu",
                 modifier = Modifier
                     .padding(start = 16.dp, end = 8.dp)
                     .size(27.dp)
+//                    .clickable(
+//                        interactionSource = remember { MustableInteractionSource() },
+//                        indication = rememberRipple(bounded = false)
+//                    ) {
+//                        // Handle menu icon click
+//                    }
             )
         },
         actions = {
             Icon(
                 imageVector = Icons.Rounded.Notifications,
-                contentDescription = null,
+                contentDescription = "View notifications",
                 modifier = Modifier
                     .padding(end = 8.dp)
                     .size(30.dp)
+//                    .clickable(
+//                        interactionSource = remember { MustableInteractionSource() },
+//                        indication = rememberRipple(bounded = false)
+//                    ) {
+//                        // Handle notifications icon click
+//                    }
             )
 
             Icon(
@@ -162,6 +152,12 @@ fun TopBar(
                 modifier = Modifier
                     .padding(start = 4.dp, end = 16.dp)
                     .size(30.dp)
+//                    .clickable(
+//                        interactionSource = remember { MustableInteractionSource() },
+//                        indication = rememberRipple(bounded = false)
+//                    ) {
+//                        // Handle profile icon click
+//                    }
             )
         }
     )
@@ -188,91 +184,103 @@ fun Screen(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun ScreenContent(paddingValues: PaddingValues){
-
-    val navController = rememberNavController()
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(paddingValues)
-            .padding(top = 0.dp),
-//        horizontalAlignment = Alignment.CenterHorizontally,
-//        contentPadding = PaddingValues(
-//            top = paddingValues.calculateTopPadding() + 10.dp
-//        )
-    ){
-        NavHost(navController, startDestination = "year_grid") {
-            composable("year_grid") { YearGridScreen(navController) }
-            composable("month_view/{year}") { backStackEntry ->
-                val year = backStackEntry.arguments?.getString("year")
-                MonthViewScreen(year)
-            }
+fun SplashScreen(navController: NavHostController) {
+    // Simulate loading with a delay
+    LaunchedEffect(Unit){
+        delay(2000)
+        navController.navigate("year_grid"){
+            popUpTo("splash"){inclusive = true}
         }
     }
 
-//    LazyColumn(
-//        modifier = Modifier.fillMaxSize(),
-//        horizontalAlignment = Alignment.CenterHorizontally,
-//        contentPadding = PaddingValues(
-//            top = paddingValues.calculateTopPadding() + 10.dp
-//        )
-//    ) {
-//        items(1100){
-//        }
-//    }
-
-//    //     code to show year grid and month view
-//    val navController = rememberNavController()
-//    NavHost(navController, startDestination = "year_grid") {
-//        composable("year_grid") { YearGridScreen(navController) }
-//        composable("month_view/{year}") { backStackEntry ->
-//            val year = backStackEntry.arguments?.getString("year")
-//            MonthViewScreen(year)
-//        }
-//    }
+    // Splash Screen UI
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Green),
+        contentAlignment = Alignment.Center
+    ) {
+        // Add your splash screen content here
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "Splash Logo",
+                    modifier = Modifier.size(200.dp)
+        )
+    }
 }
 
 @Composable
-fun ScrollContent(innerPadding: PaddingValues) {
-
+fun ScreenContent(paddingValues: PaddingValues){
+    val navController = rememberNavController()
+    NavHost(navController, startDestination = "splash") {
+        composable("splash") { SplashScreen(navController) }
+        composable("year_grid") { YearGridScreen(navController) }
+        composable("month_view/{year}") { backStackEntry ->
+            val year = backStackEntry.arguments?.getString("year")
+            MonthViewScreen(year)
+        }
+    }
 }
 
 @Composable
 fun YearGridScreen(navController: NavHostController) {
     val startYear = 1900
-    val endYear = 3000
-    val years = (startYear..endYear).toList()
+    val endYear = 2100
+    val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+    var yearsBeofre = 115
+    val yearsAfter = 116
+    val years = (currentYear - yearsBeofre..currentYear + yearsAfter).toList()
+    val lazyGridState = rememberLazyGridState()
+
+    // Scroll to the current year when the screen is first displayed
+    LaunchedEffect(Unit) {
+        val currentYearIndex = years.indexOf(currentYear)
+        if (currentYearIndex != -1) {
+            val visibleItemsCount = 4 * 7
+            val offset = (visibleItemsCount/2) - 1
+            val targetIndex = maxOf(0, currentYearIndex - offset)
+            lazyGridState.scrollToItem(targetIndex)
+        }
+    }
 
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-//        Text("Select a Year", fontSize = 24.sp, modifier = Modifier.padding(16.dp))
         LazyVerticalGrid(columns = GridCells.Fixed(4),
+            state = lazyGridState,
             contentPadding = PaddingValues(16.dp)) {
             items(years.size)
             { index ->
-                YearItem(years[index])
-                {
-                    navController.navigate("month_view/${years[index]}")
-                }
+                val year = years[index]
+                val isPastYear = year <= currentYear
+                YearItem(
+                    year = year,
+                    isPastYear = isPastYear,
+                    onClick = {
+                        navController.navigate("month_view/$year")
+                    },
+                )
             }
         }
     }
 }
 
 @Composable
-fun YearItem(year: Int, onClick: () -> Unit) {
+fun YearItem(year: Int, isPastYear: Boolean, onClick: () -> Unit) {
     Card(
         shape = RoundedCornerShape(8.dp),
         modifier = Modifier
             .padding(8.dp)
             .size(80.dp)
             .clickable { onClick() },
-        colors = CardDefaults.cardColors(containerColor = Color.LightGray)
+        colors = CardDefaults.cardColors(
+            containerColor = if (isPastYear) Color.Black else Color.LightGray)
     ) {
         Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
-            Text(text = "$year", fontSize = 16.sp)
+            Text(
+                text = "$year", fontSize = 16.sp,
+                color = if (isPastYear) Color.White else Color.Black)
         }
     }
 }
